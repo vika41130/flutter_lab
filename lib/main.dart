f211e1297591ff18b15377742035686981db65ab
vika41130 <vika41130@gmail.com>
2023-04-19 14:11:42 +0700

    CupertinoDatePicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,71 +13,42 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: CupertinoContextMenuExample(),
+      home: DatePickerExample(),
     );
   }
 }
 
-class CupertinoContextMenuExample extends StatefulWidget {
-  const CupertinoContextMenuExample({super.key});
+class DatePickerExample extends StatefulWidget {
+  const DatePickerExample({super.key});
 
   @override
-  State<CupertinoContextMenuExample> createState() =>
-      _CupertinoContextMenuExampleState();
+  State<DatePickerExample> createState() => _DatePickerExampleState();
 }
 
-class _CupertinoContextMenuExampleState
-    extends State<CupertinoContextMenuExample> {
+class _DatePickerExampleState extends State<DatePickerExample> {
+  DateTime date = DateTime(2016, 10, 26);
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text('CupertinoContextMenu  Sample'),
-        ),
-        child: Center(
-          child: SizedBox(
-            height: 100,
-            width: 100,
-            child: CupertinoContextMenu(
-                actions: <Widget>[
-                  CupertinoContextMenuAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    isDefaultAction: true,
-                    trailingIcon: CupertinoIcons.doc_on_clipboard_fill,
-                    child: const Text('Copy'),
-                  ),
-                  CupertinoContextMenuAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    trailingIcon: CupertinoIcons.share,
-                    child: const Text('Share'),
-                  ),
-                  CupertinoContextMenuAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    trailingIcon: CupertinoIcons.heart,
-                    child: const Text('Favorite'),
-                  ),
-                  CupertinoContextMenuAction(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    isDestructiveAction: true,
-                    trailingIcon: CupertinoIcons.delete,
-                    child: const Text('Delete'),
-                  ),
-                ],
-                child: Container(
-                  color: CupertinoColors.systemYellow,
-                  child: const FlutterLogo(
-                    size: 500,
-                  ),
-                )),
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoDatePicker Sample'),
+      ),
+      child: Center(
+        child: SizedBox(
+          height: 300,
+          child: CupertinoDatePicker(
+            // use24hFormat: true,
+            mode: CupertinoDatePickerMode.dateAndTime,
+            // minuteInterval: 1,
+            backgroundColor: CupertinoColors.systemGrey6,
+            onDateTimeChanged: (DateTime value) {
+              // setState(() {
+              //   date = value;
+              // });
+            },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
