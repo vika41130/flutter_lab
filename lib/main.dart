d8afd655cfecc2c9c032f7e44ff50069b8f2164f
vika41130 <vika41130@gmail.com>
2023-04-19 15:58:52 +0700

    CupertinoPicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
+const List<String> _fruitNames = <String>[
+  'Apple',
+  'Mango',
+  'Banana',
+  'Orange',
+  'Pineapple',
+  'Strawberry',
+];
+const double _kItemExtent = 32.0;
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,42 +23,64 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: DatePickerExample(),
+      home: CupertinoPickerApp(),
     );
   }
 }
 
-class DatePickerExample extends StatefulWidget {
-  const DatePickerExample({super.key});
+class CupertinoPickerApp extends StatefulWidget {
+  const CupertinoPickerApp({super.key});
 
   @override
-  State<DatePickerExample> createState() => _DatePickerExampleState();
+  State<CupertinoPickerApp> createState() => _CupertinoPickerAppState();
 }
 
-class _DatePickerExampleState extends State<DatePickerExample> {
-  DateTime date = DateTime(2016, 10, 26);
+class _CupertinoPickerAppState extends State<CupertinoPickerApp> {
+  int _selectedFruitIdx = 0;
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('CupertinoDatePicker Sample'),
-      ),
-      child: Center(
-        child: SizedBox(
-          height: 300,
-          child: CupertinoDatePicker(
-            // use24hFormat: true,
-            mode: CupertinoDatePickerMode.dateAndTime,
-            // minuteInterval: 1,
-            backgroundColor: CupertinoColors.systemGrey6,
-            onDateTimeChanged: (DateTime value) {
-              // setState(() {
-              //   date = value;
-              // });
+    return Center(
+      child: CupertinoButton(
+        onPressed: () {
+          _showDialog(CupertinoPicker(
+            magnification: 1.22,
+            squeeze: 1.2,
+            useMagnifier: true,
+            itemExtent: _kItemExtent,
+            children: List<Widget>.generate(_fruitNames.length, (index) {
+              return Center(
+                child: Text(_fruitNames[index]),
+              );
+            }),
+            onSelectedItemChanged: (int idx) {
+              setState(() {
+                _selectedFruitIdx = idx;
+              });
             },
-          ),
-        ),
+          ));
+        },
+        child: Text(_fruitNames[_selectedFruitIdx]),
       ),
     );
+  }
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 216,
+              padding: const EdgeInsets.only(top: 6.0),
+              // The Bottom margin is provided to align the popup above the system navigation bar.
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              // Provide a background color for the popup.
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              // Use a SafeArea widget to avoid system overlaps.
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
   }
 }
