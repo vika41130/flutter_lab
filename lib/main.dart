5dce6306f4c4b7bfd0142a061c53ed6b3d6219fa
vika41130 <vika41130@gmail.com>
2023-04-19 16:12:49 +0700

    CupertinoScrollbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,58 @@
 import 'package:flutter/cupertino.dart';
 
-const List<String> _fruitNames = <String>[
-  'Apple',
-  'Mango',
-  'Banana',
-  'Orange',
-  'Pineapple',
-  'Strawberry',
-];
-const double _kItemExtent = 32.0;
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: CupertinoPickerApp(),
+      home: CupertinoScrollBar(),
     );
   }
 }
 
-class CupertinoPickerApp extends StatefulWidget {
-  const CupertinoPickerApp({super.key});
+class CupertinoScrollBar extends StatefulWidget {
+  const CupertinoScrollBar({super.key});
 
   @override
-  State<CupertinoPickerApp> createState() => _CupertinoPickerAppState();
+  State<CupertinoScrollBar> createState() => _CupertinoScrollBarState();
 }
 
-class _CupertinoPickerAppState extends State<CupertinoPickerApp> {
-  int _selectedFruitIdx = 0;
+class _CupertinoScrollBarState extends State<CupertinoScrollBar> {
+  final ScrollController _controllerOne = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CupertinoButton(
-        onPressed: () {
-          _showDialog(CupertinoPicker(
-            magnification: 1.22,
-            squeeze: 1.2,
-            useMagnifier: true,
-            itemExtent: _kItemExtent,
-            children: List<Widget>.generate(_fruitNames.length, (index) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoScrollbar Sample'),
+      ),
+      child: Center(
+        child: CupertinoScrollbar(
+          thickness: 6.0,
+          thicknessWhileDragging: 10.0,
+          radius: const Radius.circular(34.0),
+          radiusWhileDragging: Radius.zero,
+          thumbVisibility: true,
+          controller: _controllerOne,
+          child: ListView.builder(
+            itemCount: 120,
+            itemBuilder: (context, index) {
               return Center(
-                child: Text(_fruitNames[index]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Item $index'),
+                ),
               );
-            }),
-            onSelectedItemChanged: (int idx) {
-              setState(() {
-                _selectedFruitIdx = idx;
-              });
             },
-          ));
-        },
-        child: Text(_fruitNames[_selectedFruitIdx]),
+          ),
+        ),
       ),
     );
-  }
-
-  void _showDialog(Widget child) {
-    showCupertinoModalPopup<void>(
-        context: context,
-        builder: (BuildContext context) => Container(
-              height: 216,
-              padding: const EdgeInsets.only(top: 6.0),
-              // The Bottom margin is provided to align the popup above the system navigation bar.
-              margin: EdgeInsets.only(
-                bottom: MediaQuery.of(context).viewInsets.bottom,
-              ),
-              // Provide a background color for the popup.
-              color: CupertinoColors.systemBackground.resolveFrom(context),
-              // Use a SafeArea widget to avoid system overlaps.
-              child: SafeArea(
-                top: false,
-                child: child,
-              ),
-            ));
   }
 }
