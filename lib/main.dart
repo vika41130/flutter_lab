e6d086d7e2dd4a2a79259449ae9c7957b5e5a12d
vika41130 <vika41130@gmail.com>
2023-04-21 16:20:04 +0700

    CupertinoTextField

--- lib/main.dart
+++ lib/main.dart
@@ -12,65 +12,56 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: TabScaffoldExample(),
+      home: CupertinoTextFieldExample(),
     );
   }
 }
 
-class TabScaffoldExample extends StatefulWidget {
-  const TabScaffoldExample({super.key});
+class CupertinoTextFieldExample extends StatefulWidget {
+  const CupertinoTextFieldExample({super.key});
 
   @override
-  State<TabScaffoldExample> createState() => _TabScaffoldExampleState();
+  State<CupertinoTextFieldExample> createState() =>
+      _CupertinoTextFieldExampleState();
 }
 
-class _TabScaffoldExampleState extends State<TabScaffoldExample> {
+class _CupertinoTextFieldExampleState extends State<CupertinoTextFieldExample> {
+  late TextEditingController _textController;
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: 'initial text');
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.search_circle_fill), label: 'Explore'),
-        ],
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoTextField Sample'),
       ),
-      tabBuilder: (context, index) {
-        return CupertinoTabView(
-          builder: (context) {
-            return CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                middle: Text('Page 1 of tab $index'),
-              ),
-              child: Center(
-                child: CupertinoButton(
-                  child: const Text('Next Page'),
-                  onPressed: () {
-                    Navigator.of(context).push(CupertinoPageRoute<void>(
-                      builder: (context) {
-                        return CupertinoPageScaffold(
-                          navigationBar: CupertinoNavigationBar(
-                            middle: Text('Page 2 of tab $index'),
-                          ),
-                          child: Center(
-                            child: CupertinoButton(
-                              child: const Text('Back'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                    ));
-                  },
-                ),
-              ),
-            );
-          },
-        );
-      },
+      child: Center(
+        child: CupertinoTextField(
+          controller: _textController,
+          placeholder: 'Enter name',
+          prefix: const Text(
+            'Name',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          decoration: const BoxDecoration(),
+          suffix: CupertinoButton(
+            child: const Icon(CupertinoIcons.delete),
+            onPressed: () {
+              _textController.clear();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
