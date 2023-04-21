78ec826f7217924254abd32a282a6ffd94f2496a
vika41130 <vika41130@gmail.com>
2023-04-21 16:05:19 +0700

    CupertinoTabScaffold

--- lib/main.dart
+++ lib/main.dart
@@ -12,45 +12,61 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: CupertinoTabBarExample(),
+      home: TabScaffoldExample(),
     );
   }
 }
 
-class CupertinoTabBarExample extends StatefulWidget {
-  const CupertinoTabBarExample({super.key});
+class TabScaffoldExample extends StatefulWidget {
+  const TabScaffoldExample({super.key});
 
   @override
-  State<CupertinoTabBarExample> createState() => _CupertinoTabBarExampleState();
+  State<TabScaffoldExample> createState() => _TabScaffoldExampleState();
 }
 
-class _CupertinoTabBarExampleState extends State<CupertinoTabBarExample> {
+class _TabScaffoldExampleState extends State<TabScaffoldExample> {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.star_fill), label: 'Favorites'),
+              icon: Icon(CupertinoIcons.home), label: 'Home'),
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.clock_solid),
-            label: 'Recents',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.person_alt_circle_fill),
-            label: 'Contacts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.circle_grid_3x3_fill),
-            label: 'Keypad',
-          ),
+              icon: Icon(CupertinoIcons.search_circle_fill), label: 'Explore'),
         ],
       ),
       tabBuilder: (context, index) {
         return CupertinoTabView(
           builder: (context) {
-            return Center(
-              child: Text('Content of tab $index'),
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text('Page 1 of tab $index'),
+              ),
+              child: Center(
+                child: CupertinoButton(
+                  child: const Text('Next Page'),
+                  onPressed: () {
+                    Navigator.of(context).push(CupertinoPageRoute<void>(
+                      builder: (context) {
+                        return CupertinoPageScaffold(
+                          navigationBar: CupertinoNavigationBar(
+                            middle: Text('Page 2 of tab $index'),
+                          ),
+                          child: Center(
+                            child: CupertinoButton(
+                              child: const Text('Back'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ));
+                  },
+                ),
+              ),
             );
           },
         );
