d5e6fd58aa096c0617c792ae2d2d57c31695bf3f
vika41130 <vika41130@gmail.com>
2023-04-21 15:18:11 +0700

    CupertinoSwitch

--- lib/main.dart
+++ lib/main.dart
@@ -12,93 +12,38 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: SliverNavBarExample(),
+      home: CupertinoSwitchExample(),
     );
   }
 }
 
-class SliverNavBarExample extends StatefulWidget {
-  const SliverNavBarExample({super.key});
+class CupertinoSwitchExample extends StatefulWidget {
+  const CupertinoSwitchExample({super.key});
 
   @override
-  State<SliverNavBarExample> createState() => _SliverNavBarExampleState();
+  State<CupertinoSwitchExample> createState() => _CupertinoSwitchExampleState();
 }
 
-class _SliverNavBarExampleState extends State<SliverNavBarExample> {
+class _CupertinoSwitchExampleState extends State<CupertinoSwitchExample> {
+  bool switchValue = true;
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          const CupertinoSliverNavigationBar(
-            leading: Icon(CupertinoIcons.person_2),
-            trailing: Icon(CupertinoIcons.add_circled),
-            largeTitle: Text('Contacts'),
-          ),
-          SliverFillRemaining(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                const Text(
-                  'Drag me up',
-                  textAlign: TextAlign.center,
-                ),
-                CupertinoButton.filled(
-                  child: const Text('Go to Next Page'),
-                  onPressed: () {
-                    Navigator.push(context, CupertinoPageRoute<Widget>(
-                      builder: (context) {
-                        return const NextPage();
-                      },
-                    ));
-                  },
-                )
-              ],
-            ),
-          )
-        ],
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoSwitch Sample'),
       ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Brightness brightness = CupertinoTheme.brightnessOf(context);
-    return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          CupertinoSliverNavigationBar(
-            backgroundColor: CupertinoColors.systemYellow,
-            border: Border(
-              bottom: BorderSide(
-                color: brightness == Brightness.light
-                    ? CupertinoColors.black
-                    : CupertinoColors.white,
-              ),
-            ),
-            // The middle widget is visible in both collapsed and expanded states.
-            middle: const Text('Contacts Group'),
-            // When the "middle" parameter is implemented, the larget title is only visible
-            // when the CupertinoSliverNavigationBar is fully expanded.
-            largeTitle: const Text('Family'),
-          ),
-          SliverFillRemaining(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const <Widget>[
-                Text('Drag me up', textAlign: TextAlign.center),
-                // When the "leading" parameter is omitted on a route that has a previous page,
-                // the back button is automatically added to the leading position.
-                Text('Tap on the leading button to navigate back',
-                    textAlign: TextAlign.center),
-              ],
-            ),
-          ),
-        ],
+      child: Center(
+        child: CupertinoSwitch(
+          activeColor: CupertinoColors.activeBlue,
+          trackColor: CupertinoColors.systemGrey,
+          thumbColor: CupertinoColors.activeOrange,
+          value: switchValue,
+          onChanged: (value) {
+            setState(() {
+              switchValue = value;
+            });
+          },
+        ),
       ),
     );
   }
